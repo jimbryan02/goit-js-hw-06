369af0104b14f187ccc27693af9e2da48ffa9ad4
jimbryan02 <113401279+jimbryan02@users.noreply.github.com>
2023-02-07 20:22:16 -0500

    last commit

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
+const colorButton = document.querySelector(".change-color");
+const colorDisplay = document.querySelector(".color");
+
+colorButton.addEventListener("click", function () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorDisplay.textContent = color;
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
